6592c7c52f9292a81e1e4faa7d1eefd0f77d82aa
Sion Williams <sion@byteshifter.io>
2017-04-12 14:26:52 +0100

    WIP

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+// Wish represents an item in a wishlist
+type Wish struct {
+	Name  string
+	Price float64
+}
+
 func WishlistScrape() {
 
 	doc, err := goquery.NewDocument("https://www.amazon.co.uk/gp/registry/wishlist/CY2FJBBFFULT")
@@ -16,9 +22,18 @@ func WishlistScrape() {
 	}
 
 	doc.Find("[id^=itemInfo]").Each(func(i int, s *goquery.Selection) {
+
 		name, _ := doc.Find("[id^=itemName]").Eq(i).Attr("title")
 		price := strings.TrimSpace(doc.Find("[id^=itemPrice]").Eq(i).Text())
-		fmt.Printf("%d Name: %s, Price: %s\n", i, name, price)
+
+		t := strings.Trim(price, "£")
+		fmt.Printf("%d Name: %s, Price: £%s\n", i, name, t)
+		//s, _ := strconv.ParseFloat(t, 64)
+		//w := &Wish{
+		//	Name:  name,
+		//	Price: s,
+		//}
+		//fmt.Print(w)
 	})
 }
 
